788f9925b88ad72b14dee83f8bb888d250d4b307
bsalcedosuarez <b.salcedo@uniandes.edu.co>
2026-04-20 11:36:43 -0500

    Revert "feat: autenticación con Google en Flutter"
    This reverts commit 8aaca4c366573b1b0b1a664737f07f08f0310384.

--- lib/services/google_id_token.dart
+++
@@ -1,5 +0,0 @@
-import 'google_id_token_io.dart'
-    if (dart.library.html) 'google_id_token_web.dart' as impl;
-
-/// Token JWT de Google (GIS en web; OAuth nativo en móvil) para Firebase.
-Future<String?> requestGoogleIdToken() => impl.requestGoogleIdToken();

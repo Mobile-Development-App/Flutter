32d753ffcdb60bb783b5196cbf47008db395d624
angelaclopez74 <angelaclopez74@gmail.com>
2026-04-23 23:55:56 -0500

    ubicacion con gps

---
+++ .dart_tool/flutter_build/326c22b00d5631f2705b82134641a68a/install_code_assets.d
@@ -0,0 +1,1 @@
+ /home/thorres/Documents/terminandoAndoProyectoConBrayitan/Flutter/.dart_tool/flutter_build/326c22b00d5631f2705b82134641a68a/native_assets.json: 
--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -46,6 +46,11 @@ public final class GeneratedPluginRegistrant {
       Log.e(TAG, "Error registering plugin flutter_plugin_android_lifecycle, io.flutter.plugins.flutter_plugin_android_lifecycle.FlutterAndroidLifecyclePlugin", e);
     }
     try {
+      flutterEngine.getPlugins().add(new com.baseflow.geolocator.GeolocatorPlugin());
+    } catch (Exception e) {
+      Log.e(TAG, "Error registering plugin geolocator_android, com.baseflow.geolocator.GeolocatorPlugin", e);
+    }
+    try {
       flutterEngine.getPlugins().add(new com.google_mlkit_barcode_scanning.GoogleMlKitBarcodeScanningPlugin());
     } catch (Exception e) {
       Log.e(TAG, "Error registering plugin google_mlkit_barcode_scanning, com.google_mlkit_barcode_scanning.GoogleMlKitBarcodeScanningPlugin", e);
